a92c3cd7e7747312bf6f568bf691d170462c8184
duongttt <duongttt1984@gmail.com>
2015-04-16 14:47:25 +0700

    second commit

--- Test1_0410.java
+++ Test1_0410.java
@@ -1,6 +1,7 @@
 package test;
 //Bài 1: Viết một hàm, có 2 đối số, trả về số lớn nhất trong 2 số truyền vào
 
+
 public class Test1_0410 {	
 	public static double compare(double x, double y) {
 		if(x < y) 
